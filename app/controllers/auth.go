268b9ef4de6972371cee96bfb3a9b6f2af7ea485
Vladimir Zaytsev <zaytsev@usc.edu>
2013-07-09 13:00:39 -0700

    Changed auth controller response format to structs instead of maps

--- app/controllers/auth.go
+++ app/controllers/auth.go
@@ -13,61 +13,77 @@ type Auth struct {
 	*revel.Controller
 }
 
+type AuthErrorResponse struct {
+	ErrorFlag    int    `json:"error"`
+	ErrorCode    int    `json:"error_code,omitempty"`
+	ErrorMessage string `json:"error_message,omitempty"`
+	ErrorDetails string `json:"error_details,omitempty"`
+}
+
+type SignInResponse struct {
+	ErrorFlag    int    `json:"error"`
+	AccountId    int64  `json:"user_id"`
+	AccountToken string `json:"user_token"`
+}
+
 func (c Auth) Signup() revel.Result {
 	var credentials models.Credentials
 
 	// Read request parameters and deserialize an instance of credentials
 	json_data, err := ioutil.ReadAll(c.Request.Body)
 	if err != nil {
-		return c.RenderJson(map[string]string{
-			"error":         "1",
-			"error_message": err.Error(),
-			"error_details": "error while reading request body",
+		return c.RenderJson(AuthErrorResponse{
+			ErrorFlag:    1,
+			ErrorMessage: "Error while reading request body.",
+			ErrorDetails: err.Error(),
 		})
 	}
+
 	if err := json.Unmarshal([]byte(json_data), &credentials); err != nil {
-		return c.RenderJson(map[string]string{
-			"error":         "1",
-			"error_message": err.Error(),
-			"error_details": "invalid json format in the request body",
+		return c.RenderJson(AuthErrorResponse{
+			ErrorFlag:    1,
+			ErrorMessage: "Invalid json format in the request body.",
+			ErrorDetails: err.Error(),
 		})
 	}
 
 	// perform validation
 	credentials.Validate(c.Validation)
 	if c.Validation.HasErrors() {
-		return c.RenderJson(map[string]string{
-			"error":         "1",
-			"error_message": "",
-			"error_details": "invalid user credentials",
+		return c.RenderJson(AuthErrorResponse{
+			ErrorFlag:    1,
+			ErrorMessage: "Invalid user credentials.",
+			ErrorDetails: err.Error(),
 		})
 	}
 
-	accountId, err := Accounts.Create(credentials)
+	accountId, err = Accounts.Create(credentials)
 
 	if err != nil {
-		return c.RenderJson(map[string]string{
-			"error":         "1",
-			"error_message": err.Error(),
-			"error_details": "API error while creating new account",
+		return c.RenderJson(AuthErrorResponse{
+			ErrorFlag:    1,
+			ErrorMessage: "API error while creating new account.",
+			ErrorDetails: err.Error(),
 		})
+
 	}
-	return c.RenderJson(map[string]string{
-		"error":           "0",
-		"success_message": "successfully performed account user signup",
+	return c.RenderJson(SignInResponse{
+		ErrorFlag:    0,
+		AccountId:    accountId,
+		AccountToken: "<token>",
 	})
 }
 
 func (c Auth) Login() revel.Result {
-	// log.Print(c.Params)
-	return c.RenderJson(map[string]string{
-		"message": "login works",
+	return c.RenderJson(AuthErrorResponse{
+		ErrorFlag:    1,
+		ErrorMessage: "Not implemented.",
 	})
 }
 
 func (c Auth) Logout() revel.Result {
-	// log.Print(c.Params)
-	return c.RenderJson(map[string]string{
-		"message": "logout works",
+	return c.RenderJson(AuthErrorResponse{
+		ErrorFlag:    1,
+		ErrorMessage: "Not implemented.",
 	})
 }
